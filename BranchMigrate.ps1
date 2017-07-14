fb976bc70b785613e494356ae7e1158413084e63
Peter McKay <pemj@users.noreply.github.com>
2017-07-14 11:16:25 -0700

    Added personal branch location

--- BranchMigrate.ps1
+++ BranchMigrate.ps1
@@ -1,26 +1,48 @@
 Param(
-	[string]$branches = $(git branch).ToString().Replace("* ", "origin/")
+	[string]$branches = "blank"
 )
 
-$upName = "upstream_new"
+$upName = "upstream_old"
 If ($(git remote -v | Select-String $upName))
 {
-	git remote -v | Select-String $upName
+	git remote set-url $upName https://msazure.visualstudio.com/DefaultCollection/One/_git/ServiceBus-DeviceHub
 }
 Else
 {
-	git remote add $upName https://msazure.visualstudio.com/DefaultCollection/One/_git/Azure-IoT-Hub-Main
+	git remote add $upName https://msazure.visualstudio.com/DefaultCollection/One/_git/ServiceBus-DeviceHub
 }
 
-#$branches = $(git branch -r | Select-String -NotMatch "\->" | Select-String "origin" ) 
 
-$branches = $branches.Split(',')
+#set the new origin
+git remote set-url origin https://msazure.visualstudio.com/DefaultCollection/One/_git/Azure-IoT-Hub-Main
 
-git pull origin
+#rename the repo root
+cd ../
+rename-item ./ServiceBus-DeviceHub Azure-IoT-Hub-Main
+cd Azure-IoT-Hub-Main
+
+
+#finagle list of branches
+git pull
+git fetch $upName
+if (branches.equals("blank")
+{
+	#get a list of this user's branches
+	$personal_branches = -split $(git for-each-ref --format='%09 %(authoremail) %09 %(refname)' | Select-String "$(git config --global user.email)" | Select-String "refs/remotes/$upName")
+	$personal_branches = $personal_branches | select-string "refs/remotes"
+	$personal_branches = foreach ($element in $personal_branches){ $element.ToString().Replace("refs/remotes", "") }
+}
+Else
+{
+	$branches = $branches.Split(',')
+}
+
+#push branches to new root
 foreach ($remote in $branches){
-	$origin, $repoName = $remote.ToString().Split('//',2)
-	Write-Host "Attempting to copy $repoName to Azure-IoT-Hub-Main."
-	#git branch --track $repoName $origin/$repoName
-	git checkout $repoName
-	git push $upName $repoName
+	$repoName, $branchName = $remote.ToString().Split('//',2)
+	Write-Host "Attempting to copy $branchName to Azure-IoT-Hub-Main."
+	git branch --track $repoName $repoName/$branchName
+	git checkout $branchName
+	git push origin $branchName
 }
+
